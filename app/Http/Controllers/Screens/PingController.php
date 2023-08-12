a58eeff3320e26c6d69828bb1d43c22416c579fd
Thiritin <me@thiritin.com>
2023-08-13 01:15:34 +0200

    Dynamic Announcements

--- app/Http/Controllers/Screens/PingController.php
+++ app/Http/Controllers/Screens/PingController.php
@@ -3,8 +3,12 @@
 namespace App\Http\Controllers\Screens;
 
 use App\Enums\ScreenStatusEnum;
+use App\Events\Broadcast\RefreshScreenEvent;
 use App\Events\Screens\FirstPingEvent;
 use App\Events\Screens\OnlineEvent;
+use App\Events\UpdateAnnouncementEvent;
+use App\Events\UpdateScheduleEvent;
+use App\Events\UpdateScreenPlaylistEvent;
 use App\Http\Controllers\Controller;
 use App\Models\Screen;
 
@@ -16,6 +20,10 @@ class PingController extends Controller
 
         if ($screen->status === ScreenStatusEnum::OFFLINE) {
             event(new OnlineEvent($screen)); // Fire the OnlineEvent event
+            if($screen->should_restart) {
+                $screen->updateQuietly(['should_restart' => false]);
+            }
+            broadcast(new RefreshScreenEvent($screen));
         }
 
         if ($screen->status === ScreenStatusEnum::UNINITIALIZED) {
